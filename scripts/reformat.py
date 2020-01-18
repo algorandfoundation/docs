f935225ea91381d0146e3ec7d7c9c78d152dab85
Will Winder <wwinder.unh@gmail.com>
2020-01-17 23:39:55 -0500

    Now it should work.

--- scripts/reformat.py
+++ scripts/reformat.py
@@ -11,7 +11,11 @@ import shutil
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
-def fix_file_links(new_file, goal_depth):
+parser = argparse.ArgumentParser(description='Reformat markdown files to display in mkdocs.')    
+parser.add_argument('-path', required=True, help='Path to reformat.')
+parser.add_argument('-cmd', required=True, help='Name of the command (goal).')
+
+def fix_file_links(new_file, goal_depth, root_files):
     """ Update markdown links to use relative paths in different directories. """
     with tempfile.NamedTemporaryFile(mode='w', dir='.', delete=False) as tmp, \
             open(new_file, 'r') as f:
@@ -24,14 +28,20 @@ def fix_file_links(new_file, goal_depth):
                     # -1 because we'll move goal.md up a directory at the end.
                     link = ('../'*goal_depth) + 'goal.md'
                 else:
-                    link = '../' + m.group(2).split('_')[-1].split('.')[0] + '/'
+                    subcommand = m.group(2).split('_')[-1].split('.')[0]
+                    if root_files != None and subcommand in root_files:
+                        link = '../' + ((subcommand + '/') * 2)
+                    else:
+                        link = '../' + subcommand + '/'
                 result = "%s%s%s" % (m.group(1), link, m.group(3))
             tmp.write(result + "\n")
         os.replace(tmp.name, new_file)
     
 
-def process(dirpath):
+def process(dirpath, cmd):
     """ move files into a directory structure and add .pages files. """
+
+    root_files = []
     files = os.listdir(dirpath)
     for f in files:
         parts = f.split('_')
@@ -46,6 +56,8 @@ def process(dirpath):
         extended_path = '_'.join(root_check) + '_'
         is_root = any(extended_path in s for s in files)
         if is_root:
+            print("Appending %s" % root_check[-1])
+            root_files.append(root_check[-1])
             parts=root_check
             root_path='/'.join(parts)
         try:
@@ -56,13 +68,15 @@ def process(dirpath):
         new_file = dirpath + '/' + root_path + '/' + new_name
         os.rename(dirpath + '/' + f, new_file)
 
-        # Rewrite file with fixed link paths
-        fix_file_links(new_file, len(parts)-1)
+        # Rewrite file with fixed link paths (except for root command)
+        if f != (cmd + '.md'):
+            fix_file_links(new_file, len(parts)-1, None)
 
         # Make sure root file is displayed first in the navigation par
         if is_root:
             with open(dirpath + '/' + root_path + '/.pages', 'w') as f:
                 f.write('arrange:\n - %s' % new_name)
+    fix_file_links(dirpath + '/' + cmd + '/' + cmd + '.md', 0, root_files)
 
 def fix_root(path):
     """ the algorithm puts everything one directory too deep, move it up. """
@@ -83,9 +97,7 @@ def fix_root(path):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Reformat markdown files to display in mkdocs.')    
-    parser.add_argument('-path', required=True, help='Path to reformat.')
-    args = parser.parse_args()    
+    args = parser.parse_args()
 
-    process(args.path)
+    process(args.path, args.cmd)
     fix_root(args.path)
