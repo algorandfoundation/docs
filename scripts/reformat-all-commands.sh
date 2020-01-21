307e2cb8437b557dfd6d636c8dbaf993a68e93e2
Will Winder <wwinder.unh@gmail.com>
2020-01-21 12:03:00 -0500

    Update driver script to regenerate goal and algokey.

---
+++ scripts/reformat-all-commands.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+./reformat.py -doc-dir ../docs/Reference-Docs/goal/ -cmd ~/go/bin/goal
+./reformat.py -doc-dir ../docs/Reference-Docs/algokey/ -cmd ~/go/bin/algokey
--- scripts/test_reformat_destructive.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-rm -rf ../docs/Reference-Docs/goal && mkdir ../docs/Reference-Docs/goal && goal generate-docs ../docs/Reference-Docs/goal && ./reformat.py -path ../docs/Reference-Docs/goal -cmd goal
