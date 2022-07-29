89205b60d2f9eb7d8e2edbd4a397dc9ceaf83803
Chris Kim <iskysun96@gmail.com>
2022-07-29 14:36:12 -0700

    changed  to  for fixed variables

--- examples/indexer/javascript/AssetsBalances.js
+++ examples/indexer/javascript/AssetsBalances.js
@@ -12,7 +12,7 @@ const indexer_port = 8980;
 let indexerClient = new algosdk.Indexer(indexer_token, indexer_server, indexer_port);
 
 (async () => {
-    let assetIndex = 2044572;
+    const assetIndex = 2044572;
     let assetInfo = await indexerClient.lookupAssetBalances(assetIndex).do();
     console.log("Information for Asset: " + JSON.stringify(assetInfo, undefined, 2));
 })().catch(e => {
--- examples/indexer/javascript/SearchTxAddressTxId.js
+++ examples/indexer/javascript/SearchTxAddressTxId.js
@@ -9,15 +9,15 @@ const indexer_port = 8980;
 
 // Instantiate the indexer client wrapper
 let indexerClient = new algosdk.Indexer(indexer_token, indexer_server, indexer_port);
-   
+
 (async () => {
-    let address = "XIU7HGGAJ3QOTATPDSIIHPFVKMICXKHMOR2FJKHTVLII4FAOA3CYZQDLG4";
-    let txid = "QZS3B2XBBS47S6X5CZGKKC2FC7HRP5VJ4UNS7LPGHP24DUECHAAA"; 
+    const address = "XIU7HGGAJ3QOTATPDSIIHPFVKMICXKHMOR2FJKHTVLII4FAOA3CYZQDLG4";
+    const txid = "QZS3B2XBBS47S6X5CZGKKC2FC7HRP5VJ4UNS7LPGHP24DUECHAAA";
     let response = await indexerClient.searchForTransactions()
         .address(address)
         .txid(txid).do();
     console.log("txid: QZS3B2XBBS47S6X5CZGKKC2FC7HRP5VJ4UNS7LPGHP24DUECHAAA = " + JSON.stringify(response, undefined, 2));
-    }  
+}
 )().catch(e => {
     console.log(e);
     console.trace();
