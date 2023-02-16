af724a3e0d18823d67dd6e778c14f9582f1af59b
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-02-16 15:41:30 -0500

    fmt

--- scripts/convert_swagger.py
+++ scripts/convert_swagger.py
@@ -10,28 +10,33 @@ import sys
 import tempfile
 import urllib.request
 
-convert_command='swagger2markup-cli.jar'
-config='swagger2markup.properties'
+convert_command = "swagger2markup-cli.jar"
+config = "swagger2markup.properties"
 
-parser = argparse.ArgumentParser(description='Generate markdown files from swagger specfile.')
-parser.add_argument('-processors', required=False, help='Optional file full of commands to fix broken swagger files. Replaces occurrances of \'{specfile}\' with the specfile')
-parser.add_argument('-target', required=True, help='Full path to output file, including the name.')
-parser.add_argument('-specfile', required=True, help='Swagger specfile.')
+parser = argparse.ArgumentParser(
+    description="Generate markdown files from swagger specfile."
+)
+parser.add_argument(
+    "-processors",
+    required=False,
+    help="Optional file full of commands to fix broken swagger files. Replaces occurrances of '{specfile}' with the specfile",
+)
+parser.add_argument(
+    "-target", required=True, help="Full path to output file, including the name."
+)
+parser.add_argument("-specfile", required=True, help="Swagger specfile.")
 
 
 def apply_commands(command_file, specfile):
     """
     Given a file full of commands, applies them line by line to the specfile, replacing the file with stdout.
     """
-    with open(command_file, 'r') as commands:
+    with open(command_file, "r") as commands:
         for command in commands:
-            command = command.strip().replace('{specfile}', specfile)
+            command = command.strip().replace("{specfile}", specfile)
             print("Running command: %s" % command)
-            with tempfile.NamedTemporaryFile(mode='w', dir='.', delete=False) as tmp:
-                subprocess.check_call([command],
-                        shell=True,
-                        cwd='.',
-                        stdout=tmp)
+            with tempfile.NamedTemporaryFile(mode="w", dir=".", delete=False) as tmp:
+                subprocess.check_call([command], shell=True, cwd=".", stdout=tmp)
                 os.replace(tmp.name, specfile)
 
 
@@ -40,16 +45,21 @@ def convert_swagger_to_markdown(specfile, directory):
     Given a specfile generate markdown files in the specified directory
     """
     if not exists(convert_command):
-        print('downloading %s...' % convert_command)
-        url='https://repo1.maven.org/maven2/io/github/swagger2markup/swagger2markup-cli/1.3.3/swagger2markup-cli-1.3.3.jar'
+        print("downloading %s..." % convert_command)
+        url = "https://repo1.maven.org/maven2/io/github/swagger2markup/swagger2markup-cli/1.3.3/swagger2markup-cli-1.3.3.jar"
         filedata = urllib.request.urlretrieve(url, convert_command)
     if not exists(config):
-        print('initializing %s...' % config)
-        with open(config, 'w') as f:
-            f.write('swagger2markup.markupLanguage=MARKDOWN')
+        print("initializing %s..." % config)
+        with open(config, "w") as f:
+            f.write("swagger2markup.markupLanguage=MARKDOWN")
 
     # Generate markdown files
-    command = 'java -jar %s convert --swaggerInput %s --config %s --outputDir %s' % (convert_command, specfile, config, directory)
+    command = "java -jar %s convert --swaggerInput %s --config %s --outputDir %s" % (
+        convert_command,
+        specfile,
+        config,
+        directory,
+    )
     subprocess.check_call([command], shell=True)
 
 
@@ -58,28 +68,32 @@ def reformat_markdown(filename, outputfilename):
     Takes output algod and kmd md files and reformats headings to look better with mkdocs.
     """
 
-    raw = open(filename, 'r').read()
+    raw = open(filename, "r").read()
 
     # Change headers to bolded so they do not show up in ToC
-    step1 = re.sub('\#\#\#\# (\S+)', r'**\1**', raw)
+    step1 = re.sub("\#\#\#\# (\S+)", r"**\1**", raw)
 
     # Make the header the description text and the request path the header so it shows up in ToC
-    step2 = re.sub('\#\#\# (.+?)\n```\n((GET|PUT|POST|DELETE|PATCH) \S+)\n```', r'### \2\n\1\n```\n\2\n```', step1)
+    step2 = re.sub(
+        "\#\#\# (.+?)\n```\n((GET|PUT|POST|DELETE|PATCH) \S+)\n```",
+        r"### \2\n\1\n```\n\2\n```",
+        step1,
+    )
 
     # Bring every header up a level
-    matches = re.finditer('^\s*#+', step2)
+    matches = re.finditer("^\s*#+", step2)
     subtract = 0
     for match in matches:
         replace = match.group()
         start = match.start()
         end = match.end()
-        step2 = step2[:start - subtract] + replace[:-1] + step2[end-subtract:]
+        step2 = step2[: start - subtract] + replace[:-1] + step2[end - subtract :]
         subtract += 1
-    outputfile = open(outputfilename, 'w')
+    outputfile = open(outputfilename, "w")
     outputfile.write(step2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.processors is not None:
@@ -89,16 +103,16 @@ if __name__ == '__main__':
         # Generate markdown files.
         convert_swagger_to_markdown(args.specfile, tmpdir)
 
-        title=args.target.split(os.sep)[-1].split('.')[0].lower()
+        title = args.target.split(os.sep)[-1].split(".")[0].lower()
         # Merge paths.md and definitions.md.
-        merged_filename=join(tmpdir, 'merged.md')
-        with open(merged_filename, 'wb') as merged:
-            merged.write(str('title: %s\n---\n' % title).encode('utf-8'))
-            for filename in ['paths.md', 'definitions.md']:
-                markdown_file=join(tmpdir, filename)
+        merged_filename = join(tmpdir, "merged.md")
+        with open(merged_filename, "wb") as merged:
+            merged.write(str("title: %s\n---\n" % title).encode("utf-8"))
+            for filename in ["paths.md", "definitions.md"]:
+                markdown_file = join(tmpdir, filename)
                 if not exists(markdown_file):
                     print("Missing '%s'" % filename)
-                with open(markdown_file, 'rb') as f:
+                with open(markdown_file, "rb") as f:
                     shutil.copyfileobj(f, merged)
 
         # Process the markdown and update target.
--- scripts/import_examples.py
+++ scripts/import_examples.py
@@ -1,68 +1,70 @@
 import os
 from dataclasses import dataclass
 
+
 @dataclass
 class ExampleSource:
     example_dir: str
-    language_name: str 
+    language_name: str
     src_comment_flag: str
     doc_comment_flag: str
 
 
 sources: list[ExampleSource] = [
     ExampleSource(
-        example_dir="../../py-algorand-sdk/_examples", 
-        language_name='python',
+        example_dir="../../py-algorand-sdk/_examples",
+        language_name="python",
         src_comment_flag="# example: ",
-        doc_comment_flag="<!-- ===PYSDK_"
+        doc_comment_flag="<!-- ===PYSDK_",
     ),
-    #ExampleSource(
-    #    example_dir="../../js-algorand-sdk/examples", 
+    # ExampleSource(
+    #    example_dir="../../js-algorand-sdk/examples",
     #    language_name='javascript',
     #    src_comment_flag="// example: ",
     #    doc_comment_flag="<!-- ===JSSDK_"
-    #),
-    #ExampleSource(
-    #    example_dir="../../go/src/github.com/algorand/go-algorand-sdk/examples", 
+    # ),
+    # ExampleSource(
+    #    example_dir="../../go/src/github.com/algorand/go-algorand-sdk/examples",
     #    language_name='go',
     #    src_comment_flag="// example: ",
     #    doc_comment_flag="<!-- ===GOSDK_"
-    #),
-    #ExampleSource(
-    #    example_dir="../../java-algorand-sdk/examples", 
+    # ),
+    # ExampleSource(
+    #    example_dir="../../java-algorand-sdk/examples",
     #    language_name='java',
     #    src_comment_flag="// example: ",
     #    doc_comment_flag="<!-- ===JAVASDK_"
-    #),
-    #ExampleSource(
-    #    example_dir="../../pyteal/examples", 
+    # ),
+    # ExampleSource(
+    #    example_dir="../../pyteal/examples",
     #    language_name='python',
     #    src_comment_flag="# example: ",
     #    doc_comment_flag="<!-- ===PYTEAL_"
-    #),
-    #ExampleSource(
-    #    example_dir="../../beaker/examples", 
+    # ),
+    # ExampleSource(
+    #    example_dir="../../beaker/examples",
     #    language_name='python',
     #    src_comment_flag="# example: ",
     #    doc_comment_flag="<!-- ===BEAKER_"
-    #)
+    # )
 ]
-
 
 
 # Example Name => source string
 SDKExamples = dict[str, list[str]]
-def find_examples_in_sdk(dir: str, prefix: str, lang: str )->SDKExamples:
+
+
+def find_examples_in_sdk(dir: str, prefix: str, lang: str) -> SDKExamples:
     directory = os.listdir(dir)
 
-    name_to_src: SDKExamples = {} 
+    name_to_src: SDKExamples = {}
     for fname in directory:
         path = os.path.join(dir, fname)
         if not os.path.isfile(path):
             name_to_src |= find_examples_in_sdk(path, prefix, lang)
         elif path[-2:] in ["py", "js", "go", "va"]:
             local_example: list[str] = []
-            with open(path, 'r') as f:
+            with open(path, "r") as f:
                 content = f.read()
                 if prefix not in content:
                     continue
@@ -70,11 +72,14 @@ def find_examples_in_sdk(dir: str, prefix: str, lang: str )->SDKExamples:
                 lines = content.splitlines()
                 for line in lines:
                     if prefix in line:
-                        name_to_src[line.strip(prefix)] = ["```"+lang, *local_example, "```"]
+                        name_to_src[line.strip(prefix)] = [
+                            "```" + lang,
+                            *local_example,
+                            "```",
+                        ]
                         local_example = []
                     else:
                         local_example.append(line)
-
 
     return name_to_src
 
@@ -88,20 +93,20 @@ def replace_matches_in_docs(dir: str, prefix: str, examples: SDKExamples):
             # recurse through directories
             replace_matches_in_docs(path, prefix, examples)
         elif path[-2:] == "md":
-            start, stop = 0,0
+            start, stop = 0, 0
             example_name = ""
             page_lines = []
-            with open(path, 'r') as f:
+            with open(path, "r") as f:
                 content = f.read()
                 if prefix not in content:
                     continue
-                    
+
                 page_lines = content.splitlines()
                 for lno, line in enumerate(page_lines):
                     if prefix in line:
                         if example_name == "":
                             example_name = line.strip(prefix + "->")
-                            start = lno+1
+                            start = lno + 1
                         else:
                             stop = lno
 
@@ -111,9 +116,9 @@ def replace_matches_in_docs(dir: str, prefix: str, examples: SDKExamples):
                     f.write("\n".join(page_lines))
 
 
-
-
 if __name__ == "__main__":
-    for src in sources: 
-        sdk_examples = find_examples_in_sdk(src.example_dir, src.src_comment_flag, src.language_name)
+    for src in sources:
+        sdk_examples = find_examples_in_sdk(
+            src.example_dir, src.src_comment_flag, src.language_name
+        )
         replace_matches_in_docs("../docs", src.doc_comment_flag, sdk_examples)
